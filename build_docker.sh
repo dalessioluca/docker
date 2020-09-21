032152501e1c65c889afe74b6948c5fa0acd78ab
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-09-21 14:10:27 +0000

    version 5

--- build_docker.sh
+++ build_docker.sh
@@ -7,5 +7,8 @@
 #docker build --tag tmp_docker:0.0.3 ./TAG_0.0.3
 #docker push us.gcr.io/broad-dsde-methods/pyro_matplotlib:0.0.2
 
-docker build --tag us.gcr.io/broad-dsde-methods/pyro_matplotlib:0.0.4 ./TAG_0.0.4
-docker push us.gcr.io/broad-dsde-methods/pyro_matplotlib:0.0.4
+#docker build --tag us.gcr.io/broad-dsde-methods/pyro_matplotlib:0.0.4 ./TAG_0.0.4
+#docker push us.gcr.io/broad-dsde-methods/pyro_matplotlib:0.0.4
+
+docker build --tag us.gcr.io/broad-dsde-methods/pyro_matplotlib:0.0.5 ./TAG_0.0.5
+docker push us.gcr.io/broad-dsde-methods/pyro_matplotlib:0.0.5
